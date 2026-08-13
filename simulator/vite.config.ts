e568d1954662585f31a43c96746bd8fb71d4788c
Kalana <kalanajinendra@gmail.com>
2026-08-13 11:10:45 +0530

    cammera feed mobile app

--- simulator/vite.config.ts
+++ simulator/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsx: 'automatic',
+    jsxImportSource: 'react',
+  },
   server: {
     port: 3000,
     open: true
